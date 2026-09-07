c26869d72a28e2106560fa0a55dfb49419f72682
Dinura-sanmith-asc <dinura.sanmith@ascentic.se>
2026-09-07 13:36:19 +0530

    Create a temporary Book model

---
+++ lib/book.dart
@@ -0,0 +1,13 @@
+class Book {
+  final int id;
+  final String title;
+  final String author;
+  final bool isAvailable;
+
+  const Book({
+    required this.id,
+    required this.title,
+    required this.author,
+    required this.isAvailable,
+  });
+}
