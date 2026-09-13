1accaac263294813d55538c698f96ed1983c7fd4
Dinura-sanmith-asc <dinura.sanmith@ascentic.se>
2026-09-14 03:31:15 +0530

    Rewrite auth_provider.dart

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -1,30 +1,76 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../data/repositories/fake_auth_repository.dart';
 import '../../domain/entities/auth_state.dart';
+import '../../domain/repositories/auth_repository.dart';
+import '../../domain/usecases/login.dart';
+import '../../domain/usecases/logout.dart';
 
-class AuthNotifier extends Notifier<AuthState> {
+final authRepositoryProvider =
+    Provider<AuthRepository>((ref) {
+  return FakeAuthRepository();
+});
+
+final loginUseCaseProvider =
+    Provider<Login>((ref) {
+  final repository = ref.watch(
+    authRepositoryProvider,
+  );
+
+  return Login(repository);
+});
+
+final logoutUseCaseProvider =
+    Provider<Logout>((ref) {
+  final repository = ref.watch(
+    authRepositoryProvider,
+  );
+
+  return Logout(repository);
+});
+
+class AuthNotifier extends AsyncNotifier<AuthState> {
   @override
-  AuthState build() {
+  Future<AuthState> build() async {
     return const AuthState(
       status: AuthStatus.loggedOut,
     );
   }
 
-  void loginAsMember() {
-    state = const AuthState(
-      status: AuthStatus.authenticated,
-      role: UserRole.member,
-    );
+  Future<void> login({
+    required String email,
+    required String password,
+  }) async {
+    state = const AsyncLoading();
+
+    state = await AsyncValue.guard(() async {
+      final login = ref.read(
+        loginUseCaseProvider,
+      );
+
+      return login(
+        email: email,
+        password: password,
+      );
+    });
   }
 
-  void logout() {
-    state = const AuthState(
-      status: AuthStatus.loggedOut,
+  Future<void> logout() async {
+    final logout = ref.read(
+      logoutUseCaseProvider,
+    );
+
+    await logout();
+
+    state = const AsyncData(
+      AuthState(
+        status: AuthStatus.loggedOut,
+      ),
     );
   }
 }
 
 final authProvider =
-    NotifierProvider<AuthNotifier, AuthState>(
+    AsyncNotifierProvider<AuthNotifier, AuthState>(
   AuthNotifier.new,
 );
